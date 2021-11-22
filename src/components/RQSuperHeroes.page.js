fd1f93d9e41dc3c9f2b60983c0e3179208fc0f3f
raymankatib <rayman.katib@gmail.com>
2021-11-22 09:38:55 -0500

    test react-query interval with state update

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,19 +1,32 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { useState } from 'react';
 
 const fetchSpuerHeroes = () => {
 	return axios.get('http://localhost:4000/superheroes');
 };
 
 export const RQSuperHeroesPage = () => {
+	const [intervalValue, setIntervalValue] = useState(3000);
+
+	const onSuccess = ({ data }) => {
+		console.log('side effect after fetch', data);
+		if (data.length === 4) setIntervalValue(false);
+	};
+	const onError = (error) => {
+		console.log('side effect after Error', error);
+	};
+
 	const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heroes', fetchSpuerHeroes, {
 		// cacheTime: 5000,
 		// staleTime: 5000
 		// refetchOnMount: true, // "Refetch every time the component mount"
 		// refetchOnWindowFocus: true // "Default value is true it does refetch when we remove the focus from the tab"
-		// refetchInterval: 2000,
+		refetchInterval: intervalValue,
 		// refetchIntervalInBackground: true // "interval works even if the component is not focused",
-		enabled: false // to stop fetching on mount to works only one user event
+		// enabled: false // to stop fetching on mount to works only one user event
+		onSuccess,
+		onError
 	});
 	// isFetching for the updated value if cached data changed
 	// refetch is event to call the query example "<button onClick={refetch}>Fetch Data</button>""
